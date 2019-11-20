0e0d7b23cd683b601c0ffc50d900b633682f52bc
bakenray <lpqstrong@163.com>
2019-11-20 16:52:30 +0800

    ruby case 语句

---
+++ ruby-basic/9-case.rb
@@ -0,0 +1,12 @@
+# case语句
+case_value = 5
+case_result = case case_value
+    when 1 
+        '1' 
+    when 2,3 
+        '2 or 3' 
+    else 
+        'value is so big' 
+    end
+
+p case_result
