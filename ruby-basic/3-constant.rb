c2348f69ae341f492f47e267a7a2a22baa9113b0
bakenray <lpqstrong@163.com>
2019-11-20 14:37:23 +0800

    ruby 常量

--- ruby-basic/3-constant.rb
+++ ruby-basic/3-constant.rb
@@ -1,6 +1,8 @@
 # 常量，使用全大写的字母来声明
 CONSTANT_1 = 'CONSTANT_1'
 def ruby_method
-    p CONSTANT_1
+    p CONSTANT_1    #自定义的常量
+    p RUBY_VERSION  #ruby 自带常量，查看ruby版本
+    p ARGV          #ruby 自带常量，ruby命令执行时带的参数
 end
 ruby_method
