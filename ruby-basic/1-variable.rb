5640b5cc569b9f6ee44a8fcec40428f0899041b2
bakenray <lpqstrong@163.com>
2019-11-20 14:19:58 +0800

    ruby 变量

---
+++ ruby-basic/1-variable.rb
@@ -0,0 +1,15 @@
+# 变量，没有关键字，直接声明并赋值
+my_variable = 'variable'
+
+# 局部变量，小写字母开头 或者 下划线开头_
+local_variable = 'local_variable_1'
+_local_variable = 'local_variable_2'
+
+# 全局变量，美元符号$开头
+$global_variable = 'global_variable'
+
+# 实例变量，一个@符号开头
+@instance_variable = 'instance_variable'
+
+# 类变量，两个@@符号开头
+@@class_variable = 'class_variable'
